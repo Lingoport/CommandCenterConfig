158f87b7229e63592947b80e95a497195705e109
Olivier Libouban <olibouban@lingoport.com>
2023-08-31 09:28:45 -0600

    Removed troubleshooting echo

--- transforms/Defaults2Localized/transform_from_repo.sh
+++ transforms/Defaults2Localized/transform_from_repo.sh
@@ -13,7 +13,6 @@
           translated_dir="$(dirname "$base_dir")/LocalizedResources"
           mkdir -p "$translated_dir"
           set -x
-          echo "rsync -a -v  ${base_dir} ${translated_dir}"
           rsync -a -v  "$base_dir/" "$translated_dir/"
           set +x
 	fi
