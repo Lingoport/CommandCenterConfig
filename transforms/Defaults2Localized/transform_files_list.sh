b568681ee4f6b4cbedd8fbe256c34575c6661ab5
Olivier Libouban <olibouban@lingoport.com>
2023-08-31 11:22:38 -0600

    Only rm LRM Tags from files pushed to the repository

--- transforms/Defaults2Localized/transform_files_list.sh
+++ transforms/Defaults2Localized/transform_files_list.sh
@@ -1,2 +1,21 @@
 #!/bin/bash
-# No-Op
+# Remove any LRM tag from 'incoming' files
+#
+
+echo "The following files are about to be added to the repository"
+echo "This script removes any LRM tag from those files"
+cat "$1"
+
+cat "$1" | while read -r FILEPATH
+do
+  echo " sed rm LRM Tag line in [${FILEPATH}]"
+
+  # If a line has FULL-FILE, remove it
+  sed -i '/FULL-FILE/d' "$FILEPATH"
+
+  # If a line has CHANGES-ONLY, remove it
+  sed -i '/CHANGES-ONLY/d' "$FILEPATH"
+
+done
+echo  " ------------------------------------------------"
+echo " "
