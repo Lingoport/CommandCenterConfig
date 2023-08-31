68ff55605c1512d4023f510be4dea5212a427bea
Olivier Libouban <olibouban@lingoport.com>
2023-08-31 09:09:14 -0600

    If then fi

--- transforms/Defaults2Localized/transform_from_repo.sh
+++ transforms/Defaults2Localized/transform_from_repo.sh
@@ -7,13 +7,13 @@
 	# Watch out: If base_dir is empty, it would try to
 	# rsync from /  (root), i.e. the entire system !
 	if [[ ! -z "$base_dir" ]] 
-	do
+	then
           translated_dir="$(dirname "$base_dir")/LocalizedResources"
           mkdir -p "$translated_dir"
           set -x
           echo "rsync -a -v  ${base_dir} ${translated_dir}"
           #rsync -a -v  "$base_dir/" "$translated_dir/"
           set +x
-        done
+	fi
     done <<< "$(find . -path "*/UnifiedResources/DefaultResources")"
 )
