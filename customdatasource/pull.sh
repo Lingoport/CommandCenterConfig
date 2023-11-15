777d7c208fbfee75e546d7a14434a18746a196a6
mheilnerLingoport <mheilner@lingoport.com>
2023-11-15 12:40:52 -0700

    Update pull.sh

--- customdatasource/pull.sh
+++ customdatasource/pull.sh
@@ -1,1 +1,38 @@
+#!/bin/bash
+
+echo ""
+echo "Custom Pull Incoming variables:"
+echo "     CUSTOM_DIR         = ${CUSTOM_DIR}"
+echo "     CLIENT_SOURCE_DIR  = ${CLIENT_SOURCE_DIR}"
+
+# cd to workspaces directory
+cd "${CLIENT_SOURCE_DIR}"
+
+# set these
+svnUsername="svnuser"
+svnPassword="c1sY,^cszi,%Qi"
+
+echo "svn update --no-auth-cache --non-interactive --username ${svnUsername} --password xxxxx"
+svn update --no-auth-cache --non-interactive --username ${svnUsername} --password ${svnPassword} > /dev/null 2>&1
+exitValue=$?
+if [ $exitValue != "0" ];then
+    echo "Error: svn update failed with status: $exitValue"
+    echo "Pull script FAILED"
+    exit $exitValue;
+fi
+
+echo "svn status"
+svn status
+
+echo "svn info"
+svn info
+
+conflicts=`svn status | grep '^C'`
+if [ -z "$conflicts" ]; then 
+    echo "Pull script completed successfully"
+    exit 0
+else  
+    echo "Pull script failed due to conflicts "
+    exit 1
+fi
 
