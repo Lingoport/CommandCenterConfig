956bf4408f9e0441bf53ef663152c66f2fc2b541
mheilnerLingoport <mheilner@lingoport.com>
2023-11-15 12:37:04 -0700

    Update clone.sh

--- customdatasource/clone.sh
+++ customdatasource/clone.sh
@@ -1,1 +1,38 @@
+#!/bin/bash
+
+
+
+echo "Custom Clone Incoming variables:"
+echo "    CUSTOM_DIR          = ${CUSTOM_DIR}"
+echo "    WORKSPACES_DIR      = ${WORKSPACES_DIR}"
+echo "    WORKSPACE_NAME      = ${WORKSPACE_NAME}"
+
+# cd to workspaces directory
+cd ${WORKSPACES_DIR}
+
+# set these
+svnUsername="svnuser"
+svnPassword="c1sY,^cszi,%Qi"
+svnUrl="https://svn.lingoport.io/svn/Test/AliciaProject/branches/testbranch"
+
+
+# checkout
+echo "svn checkout --no-auth-cache --non-interactive --username ${svnUsername} --password xxxxx ${svnUrl} ${WORKSPACE_NAME}"
+svn checkout --no-auth-cache --non-interactive --username ${svnUsername} --password ${svnPassword} ${svnUrl} ${WORKSPACE_NAME} > /dev/null 2>&1
+exitValue=$?
+if [ $exitValue != "0" ];then
+    echo "Error: svn checkout failed with status: $exitValue"
+    echo "Checkout script FAILED"
+    exit $exitValue;
+fi
+	
+echo "svn status"
+svn status
+
+echo "svn info"
+svn info
+
+echo "Checkout script completed successfully"
+exit 0
+
 
