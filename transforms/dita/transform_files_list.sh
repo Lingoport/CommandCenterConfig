90ed26ceac89d57bff8a00a24ad9de340df320e8
Olivier Libouban <oliboubanLingoport@users.noreply.github.com>
2023-12-18 15:57:04 -0700

    Update transform_files_list.sh

--- transforms/dita/transform_files_list.sh
+++ transforms/dita/transform_files_list.sh
@@ -1,2 +1,10 @@
 #!/bin/bash
 echo "TO BE WRITTEN. For now, here so we can try out the first pass of transform_from_repo"
+
+echo "Starter to get a sense for the files at work"
+echo  " ------------------------------------------------"
+echo  " Current pwd:"
+pwd
+
+echo "Files to transform: " 
+cat "${FULL_LIST_PATH}"
