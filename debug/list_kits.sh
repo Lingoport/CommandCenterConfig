1f54356e18a36dc8be66d3e562a55f3bc12a938d
Olivier Libouban <olibouban@lingoport.com>
2024-01-04 09:33:03 -0700

    Example debug script using system variables

---
+++ debug/list_kits.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+echo "Modify this example Debug script to your needs"
+echo " "
+echo "Sample Debug Script, using system variables:
+echo " * DEBUG_GN : the group name of a project"
+echo " * DEBUG_PN : the project name itself"
+echo " * DEBUG_MN : the module name"
+echo ""
+echo "This script lists the content of :
+echo " * The files sent to translation"
+echo " * The files received from translation correctly"
+echo " * The failed translations"
+
+echo " * DEBUG_GN=${DEBUG_GN}"
+echo " * DEBUG_PN=${DEBUG_PN}"
+echo " * DEBUG_MN=${DEBUG_MN}"
+
+# Check that at least DEBUG_GN and DEBUG_PN are not empty!
+if [ -z "${DEBUG_GN}" ]
+then
+      echo "\$DEBUG_GN is empty: exiting!"
+      exit -1
+fi
+
+if [ -z "${DEBUG_PN}" ]
+then
+      echo "\$DEBUG_PN is empty: exiting!"
+      exit -1
+fi
+
+# If MN is empty, then don't append that to the location of the project directory
+if [ -z "${DEBUG_MN}" ]
+then
+  DEBUG_DIR="/usr/local/tomcat/Lingoport_Data/L10nStreamlining/${DEBUG_GN}/projects/${DEBUG_PN}"
+else
+  DEBUG_DIR="/usr/local/tomcat/Lingoport_Data/L10nStreamlining/${DEBUG_GN}/projects/${DEBUG_PN}_${DEBUG_MN}"
+
+fi
+
+echo " * DEBUG_DIR=${DEBUG_DIR}"
+
+echo " Listing for kits sent to translation:"
+echo " -------------------------------------"
+ls -l "${DEBUG_DIR}"/prepkits/PREP_KIT*/*/*.*
+
+echo " Listing for kits received from translation:"
+echo " --------------------------------------------"
+ls -l "${DEBUG_DIR}"/importkits/TRANSLATED_KIT*/*/*.*
+
+echo " Listing for kits sent to translation:"
+echo " -------------------------------------"
+ls -l "${DEBUG_DIR}"/failedkits/TRANSLATED_KIT_*/*/*.*
+
+echo " ====================================================="
+
+
