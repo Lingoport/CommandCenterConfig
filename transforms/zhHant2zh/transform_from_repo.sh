5ec9c8c4f4faa494fce56382f71f490444d05969
Olivier Libouban <olibouban@lingoport.com>
2024-01-15 11:35:17 -0700

    Removing the echoes, the real copy/add/commit/push.

--- transforms/zhHant2zh/transform_from_repo.sh
+++ transforms/zhHant2zh/transform_from_repo.sh
@@ -1,22 +1,2 @@
 #!/bin/bash
-#
-
-cd "$CLIENT_SOURCE_DIR"
-
-find . -name zh-Hant.lproj > "${PROJECT_TMP_DIR}/zhHant.txt"
-
-while read hantDir; do
-  echo "$hantDir"
-  test=`dirname "$hantDir"`
-  zhDir="${test}/zh.lproj"
-  echo "mkdir -p ${zhDir}"
-  echo "cp ${hantDir}/* ${zhDir}"
-  echo "git add ${zhDir}/*"
-done < "${PROJECT_TMP_DIR}/zhHant.txt"
-
-git status
-echo "git commit -m\"Lingoport Sync with zh-Hant and zh\" . || true"
-echo "Done with Copying Hant to zh"
-echo "git push"
-echo "git status"
-
+# No Op
--- transforms/zhHant2zh/transform_to_repo.sh
+++ transforms/zhHant2zh/transform_to_repo.sh
@@ -6,17 +6,18 @@ cd "$CLIENT_SOURCE_DIR"
 find . -name zh-Hant.lproj > "${PROJECT_TMP_DIR}/zhHant.txt"
 
 while read hantDir; do
-  echo "$hantDir"
   test=`dirname "$hantDir"`
   zhDir="${test}/zh.lproj"
-  echo "mkdir -p ${zhDir}"
-  echo "cp ${hantDir}/* ${zhDir}"
-  echo "git add ${zhDir}/*"
-done < zhHant.txt
+  mkdir -p "${zhDir}"
+  cp "${hantDir}"/* "${zhDir}"
+  git add "${zhDir}"/*
+done < "${PROJECT_TMP_DIR}/zhHant.txt"
 
 git status
-echo "git commit -m\"Lingoport Sync with zh-Hant and zh\" . || true"
-echo "Done with Copying Hant to zh"
-echo "git push"
-echo "git status"
+git commit -m"Lingoport Sync with zh-Hant and zh" . || true"
+echo " - Copied zh-Hant.lproj content to zh.lproj"
+git push
+git status
+echo " END $0"
+echo " ----------------------------------------------"
 
