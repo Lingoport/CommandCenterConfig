75fa8b99eed098c232d96c4fc5ece52c319692bc
Peter Rosenthal <prosenthal@lingoport.com>
2024-01-26 15:09:22 -0700

    Update gerrit_cleanup.sh

--- debug/gerrit/gerrit_cleanup.sh
+++ debug/gerrit/gerrit_cleanup.sh
@@ -8,20 +8,6 @@ echo "    PROJECT_ID=${PROJECT_ID}"
 echo "    SHA_TO_CLEAN=${SHA_TO_CLEAN}"
 echo ""
 
-if [ -z "${PROJECT_ID}" ]; then
-    echo "PROJECT_ID is empty"
-    echo "To set system variables like PROJECT_ID in Command Center, navigate to Settings > Advanced Settings > System Environment Variables"
-    echo "Gerrit cleanup FAILED"
-    exit -1
-if
-
-if [ -z "${SHA_TO_CLEAN}" ]; then
-    echo "PROJECT_ID is empty"
-    echo "To set system variables like PROJECT_ID in Command Center, navigate to Settings > Advanced Settings > System Environment Variables"
-    echo "Gerrit cleanup FAILED"
-    exit -1
-if
-
 echo "$ cd /usr/local/tomcat/Lingoport_Data/CommandCenter/system/gerritfiles/project/${PROJECT_ID}"
 cd "/usr/local/tomcat/Lingoport_Data/CommandCenter/system/gerritfiles/project/${PROJECT_ID}"
 
