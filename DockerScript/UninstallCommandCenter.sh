07b481812b1122204bdda7dfa0a0928526fa4f88
ljiLingoport <lji@lingoport.com>
2024-02-24 17:28:44 -0800

    Update UninstallCommandCenter.sh

--- DockerScript/UninstallCommandCenter.sh
+++ DockerScript/UninstallCommandCenter.sh
@@ -20,19 +20,16 @@ if [[ -e "install.conf" ]]; then
     echo "Reading configured information from install.conf file."
 fi
 
-# Ensure the backup directory exists
-mkdir -p $home_directory/commandcenter/backup || true
-
 # Change to the config directory
 cd $home_directory/commandcenter/config || { echo "Failed to change to config directory"; exit 1; }
 
-# Read the container ID and stop/remove the container
+# Read the container ID and stop the container
 oldID=$(cat cc_container_id.txt)
-sudo docker stop $oldID && sudo docker rm $oldID || { echo "Failed to stop/remove Docker container $oldID"; exit 1; }
+sudo docker stop $oldID || { echo "Failed to stop Docker container $oldID"; exit 1; }
 
-# Read the database container ID and stop/remove the container
+# Read the database container ID and stop the container
 db=$(cat cc_mysql_id.txt)
-sudo docker stop $db && sudo docker rm $db || { echo "Failed to stop/remove Docker database container $db"; exit 1; }
+sudo docker stop $db || { echo "Failed to stop Docker database container $db"; exit 1; }
 
 rm -rf $home_directory/commandcenter/logs/*
 rm -rf $home_directory/Lingoport_Data/*
