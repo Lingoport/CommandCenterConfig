6dd8c5b8ea7b0000d77e54e367aafff2c65fe8d3
Lingoport CI for QA <lpdevLingoport@lingoport.com>
2024-03-05 16:49:01 -0700

    Update bug8334.sh

--- debug/bug8334.sh
+++ debug/bug8334.sh
@@ -5,6 +5,7 @@ echo "     MARY_VAR = ${MARY_VAR}"
 echo "     DITA_DIR = ${DITA_DIR}"
 env
 
+echo " You should see this change!"
 echo "      LINGOPORT_HOME=${LINGOPORT_HOME}
 java -jar ${LINGOPORT_HOME}/lrm-server-10.0/lrm-cli.jar -v
 java -jar ${LINGOPORT_HOME}/globalyzer-lite-6.8.0_6.0/globalyzer-lite.jar -v
