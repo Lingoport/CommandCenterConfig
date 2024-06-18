f9055b25beea60f1b316c6ccf81c977008319174
Olivier Libouban <oliboubanLingoport@users.noreply.github.com>
2024-06-18 11:09:45 -0600

    Only keep files with key/value pairs, remove files without

--- transforms/resx_specific_keys/transform_from_repo.sh
+++ transforms/resx_specific_keys/transform_from_repo.sh
@@ -17,8 +17,16 @@ cat "${PROJECT_TMP_DIR}/input_files.txt"
 
 cat "${PROJECT_TMP_DIR}/input_files.txt" | while read -r resxfile
 do
+  #xmlstarlet ed -d '//data[(starts-with(@name, ">>")) or (@type)]' "$resxfile" > "${resxfile}textonly"
+  xmlstarlet ed -d '//data[(starts-with(@name, ">>")) or (@type) or (@mimetype)] | //metadata' "$resxfile" > "${resxfile}textonly"
 
-  #xmlstarlet ed -d '//data[not(contains(@name, ".Text"))]' "$resxfile" > "${resxfile}textonly"
-  xmlstarlet ed -d '//data[(starts-with(@name, ">>")) or (@type)]' "$resxfile" > "${resxfile}textonly"
+  # Remove 16 lines from "Example:" in the textonly file, as they include <data> and </data> as examples
+  sed -i '/Example:/,+15d' "${resxfile}textonly"
 
+  # Finally, remove that file if there are not key/value pairs, hopefully identified by no </data>
+  grep "<\data>" "${resxfile}textonly"
+  if [[ $? -ne 0 ]]; then
+    echo "No key/value in ${resxfile}textonly, removing it"
+    rm "${resxfile}textonly"
+  fi
 done
