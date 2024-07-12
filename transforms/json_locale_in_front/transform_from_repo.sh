60ec6dc07b23bbf958eaae4fa92b00d93c06912c
Olivier Libouban <olibouban@lingoport.com>
2024-07-12 09:29:52 -0600

    first pass at transforms for en_bell.json -> bell_en.json, etc.

---
+++ transforms/json_locale_in_front/transform_from_repo.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Specify the directory to search for files
+directory="bell"
+
+# Change to the specified directory
+cd "$directory" || exit
+
+# Find all .json files that match the pattern *_*.json
+for old_filename in *_*.json; do
+    # Check if the file exists
+    if [[ -e "$old_filename" ]]; then
+        # Extract the part before the last underscore and the part after the last underscore
+        before_last_underscore="${old_filename%_*}"
+        after_last_underscore="${old_filename##*_}"
+        
+        # Form the new filename
+        new_filename="${after_last_underscore%.json}_${before_last_underscore}.json"
+        
+        # Rename the file
+        cp "$old_filename" "$new_filename"
+        echo "Moved: $old_filename to $new_filename"
+    fi
+done
---
+++ transforms/json_locale_in_front/transform_to_repo.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# Specify the directory to search for files
+directory="bell"
+
+# Change to the specified directory
+cd "$directory" || exit
+
+# Find all .json files that match the pattern *_*.json
+for old_filename in *_*.json; do
+    # Check if the file exists
+    if [[ -e "$old_filename" ]]; then
+
+        # Extract the part before the first underscore and the part after the first underscore
+        before_underscore="${old_filename%%_*}"
+        
+        # Extract the part after the first underscore and before the .json extension
+        after_underscore="${old_filename#*_}"
+        base_name="${after_underscore%.json}"
+        
+        # Form the new filename
+        new_filename="${base_name}_${before_underscore}.json" 
+        # Rename the file
+        mv -f "$old_filename" "$new_filename"
+        echo "Renamed: $old_filename to $new_filename"
+    fi
+done
+
