d0978f1d849d0491e8264ee88db9be5afe43e153
Peter Rosenthal <prosenthal@lingoport.com>
2024-07-24 14:35:08 -0600

    Update tractor_merge_fix.sh

--- debug/gerrit/tractor_merge_fix.sh
+++ debug/gerrit/tractor_merge_fix.sh
@@ -1,3 +1,2 @@
 cd /usr/local/tomcat/Lingoport_Data/CommandCenter/workspaces/a.tractor.dev
-git cherry-pick a990ebc33cad9e740e89fff9d9b227880ef10e29
-cat tractorapp/src/main/res/values-xx/strings.xml
+git status
