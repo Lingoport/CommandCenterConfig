20b1e233701e9cdd361302a52854d3400449d536
Peter Rosenthal <prosenthal@lingoport.com>
2024-07-24 14:50:06 -0600

    Update tractor_merge_fix.sh

--- debug/gerrit/tractor_merge_fix.sh
+++ debug/gerrit/tractor_merge_fix.sh
@@ -2,21 +2,25 @@ echo "$ cd /usr/local/tomcat/Lingoport_Data/CommandCenter/workspaces/a.tractor.d
 cd /usr/local/tomcat/Lingoport_Data/CommandCenter/workspaces/a.tractor.dev
 
 echo ""
-echo "git reset --hard b5c027019"
+echo "$ git reset --hard b5c027019"
 git reset --hard b5c027019
 
 echo ""
-echo "git status"
+echo "$ git status"
 git status
 
 echo ""
-echo "git cherry-pick a990ebc33cad9e740e89fff9d9b227880ef10e29 --strategy=theirs"
+echo "$ git cherry-pick a990ebc33cad9e740e89fff9d9b227880ef10e29 --strategy=theirs"
 git cherry-pick a990ebc33cad9e740e89fff9d9b227880ef10e29 --strategy=theirs
 
 echo ""
-echo "git cherry-pick --continue"
+echo "$ git cherry-pick --continue"
 git cherry-pick --continue
 
 echo ""
-echo "git status"
+echo "$ git cherry-pick --continue"
+git cherry-pick --continue
+
+echo ""
+echo "$ git status"
 git status
