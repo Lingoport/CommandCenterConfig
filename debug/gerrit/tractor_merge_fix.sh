58d4712cb14d5c5059800f4159241844da370864
Peter Rosenthal <prosenthal@lingoport.com>
2024-08-06 10:59:33 -0600

    add powermanager git checks

--- debug/gerrit/tractor_merge_fix.sh
+++ debug/gerrit/tractor_merge_fix.sh
@@ -2,5 +2,14 @@ echo "$ cd /usr/local/tomcat/Lingoport_Data/CommandCenter/workspaces/a.powermana
 cd /usr/local/tomcat/Lingoport_Data/CommandCenter/workspaces/a.powermanager.dev
 
 echo ""
+echo "$ git checkout develop"
+git checkout develop
+
+echo "$ git fetch origin"
+git fetch origin
+
+echo "$ git pull"
+git pull
+
 echo "$ git status"
 git status
