59df7ceba553403e1fa567d633fe133ac1faa72d
Peter Rosenthal <prosenthal@lingoport.com>
2024-08-06 09:59:36 -0600

    new gerrit repo tests
    not necessarily for tractor anymore, but still same concept so I'm modifying the same script

--- debug/gerrit/tractor_merge_fix.sh
+++ debug/gerrit/tractor_merge_fix.sh
@@ -1,22 +1,6 @@
-echo "$ cd /usr/local/tomcat/Lingoport_Data/CommandCenter/workspaces/a.tractor.dev"
-cd /usr/local/tomcat/Lingoport_Data/CommandCenter/workspaces/a.tractor.dev
-
-echo ""
-echo "$ git reset --hard b5c027019"
-git reset --hard b5c027019
+echo "$ cd /usr/local/tomcat/Lingoport_Data/CommandCenter/workspaces/a.powermanager.dev"
+cd /usr/local/tomcat/Lingoport_Data/CommandCenter/workspaces/a.powermanager.dev
 
 echo ""
 echo "$ git status"
 git status
-
-echo ""
-echo "$ git cherry-pick a990ebc33cad9e740e89fff9d9b227880ef10e29 --strategy-option=theirs"
-git cherry-pick a990ebc33cad9e740e89fff9d9b227880ef10e29 --strategy-option=theirs
-
-echo ""
-echo "$ git status"
-git status
-
-echo ""
-echo "$ cat tractorapp/src/main/res/values-xx/strings.xml"
-cat tractorapp/src/main/res/values-xx/strings.xml
