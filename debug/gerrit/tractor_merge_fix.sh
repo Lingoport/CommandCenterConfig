54b21497e632f9404909ec2a3deca60bfaa1cb3d
Peter Rosenthal <prosenthal@lingoport.com>
2024-08-07 10:05:14 -0600

    retrieve gerritclone and gerritpull scripts through debug script

--- debug/gerrit/tractor_merge_fix.sh
+++ debug/gerrit/tractor_merge_fix.sh
@@ -4,5 +4,14 @@ echo $PROSENTHAL_PAT
 echo "$ cd /usr/local/tomcat/scripts"
 cd /usr/local/tomcat/scripts
 
+echo "$ git clone https://prosenthalLingoport:$PROSENTHAL_PAT@github.com/Lingoport/Command-Center.git temp"
+git clone https://prosenthalLingoport:$PROSENTHAL_PAT@github.com/Lingoport/Command-Center.git temp
+
+echo "$ cp temp/gerritclone.sh ."
+cp temp/gerritclone.sh .
+
+echo "$ cp temp gerritpull.sh ."
+cp temp/gerritpull.sh .
+
 echo "$ ls -la"
 ls -la
