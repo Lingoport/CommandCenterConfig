3dcf91d4590e94e3be6aad681d3e81b3340b2a58
Peter Rosenthal <prosenthal@lingoport.com>
2024-08-07 10:01:45 -0600

    Update tractor_merge_fix.sh

--- debug/gerrit/tractor_merge_fix.sh
+++ debug/gerrit/tractor_merge_fix.sh
@@ -1,8 +1,8 @@
 echo "$ echo \$PROSENTHAL_PAT"
 echo $PROSENTHAL_PAT
 
-echo "$ cd /usr/local/tomcat/"
-cd /usr/local/tomcat/
+echo "$ cd /usr/local/tomcat/scripts"
+cd /usr/local/tomcat/scripts
 
 echo "$ ls -la"
 ls -la
