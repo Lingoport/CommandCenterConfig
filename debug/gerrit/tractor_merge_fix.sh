70af489a3594d89665292698fb7e2090d5f7e25d
Peter Rosenthal <prosenthal@lingoport.com>
2024-08-07 09:58:08 -0600

    Update tractor_merge_fix.sh

--- debug/gerrit/tractor_merge_fix.sh
+++ debug/gerrit/tractor_merge_fix.sh
@@ -1,5 +1,5 @@
-echo "$ cd /usr/local/tomcat/Lingoport_Data/CommandCenter/system"
-cd /usr/local/tomcat/Lingoport_Data/CommandCenter/system
+echo "$ cd /usr/local/tomcat/Lingoport_Data/CommandCenter/system/workspaces"
+cd /usr/local/tomcat/Lingoport_Data/CommandCenter/system/workspaces
 
 echo "$ ls -la"
 ls -la
