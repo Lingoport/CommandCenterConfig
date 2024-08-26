0dcdc3d90c7cc720f6c098c223292adb30258fa0
Olivier Libouban <olibouban@lingoport.com>
2024-08-26 15:18:24 -0600

    Cut and Paste name of file to iterate

--- transforms/CV/transform_to_repo.sh
+++ transforms/CV/transform_to_repo.sh
@@ -8,7 +8,7 @@ echo "Find all properties before removing the comments from the target files"
 find . -name "*\.properties" -type f > "${PROJECT_TMP_DIR}/properties_files.txt"
 
 echo "Removes all the comments from non English files"
-sed -i '/en.properties/d' ~/tmp/input_files.txt
+sed -i '/en.properties/d' "${PROJECT_TMP_DIR}/properties_files.txt"
 cat "${PROJECT_TMP_DIR}/properties_files.txt"| while read -r FILEPATH
 do
  sed -i '/^#/d' $FILEPATH
