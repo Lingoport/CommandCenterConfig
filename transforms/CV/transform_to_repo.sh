3f16e6364a2dfc209a5e5252167385c6726c18f4
Olivier Libouban <olibouban@lingoport.com>
2024-08-26 15:05:01 -0600

    Controlled Vocabulary: No Comments in Translated Files

---
+++ transforms/CV/transform_to_repo.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# Removes comments from non English files.
+#
+# From the WORKSPACE, find all the non English files
+# and each such file, sed the comments out of the file
+
+echo "Find all properties before removing the comments from the target files"
+find . -name "*\.properties" -type f > "${PROJECT_TMP_DIR}/properties_files.txt"
+
+echo "Removes all the comments from non English files"
+sed -i '/en.properties/d' ~/tmp/input_files.txt
+cat "${PROJECT_TMP_DIR}/properties_files.txt"| while read -r FILEPATH
+do
+ sed -i '/^#/d' $FILEPATH
+done
+
