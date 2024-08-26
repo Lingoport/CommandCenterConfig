124774ce6164541e22f3a9d45d68ef6c8f89ee6b
Olivier Libouban <olibouban@lingoport.com>
2024-08-26 15:29:13 -0600

    Take only those files of interest at that moment

--- transforms/CV/transform_to_repo.sh
+++ transforms/CV/transform_to_repo.sh
@@ -5,11 +5,13 @@
 # and each such file, sed the comments out of the file
 
 echo "Find all properties before removing the comments from the target files"
-find . -name "*\.properties" -type f > "${PROJECT_TMP_DIR}/properties_files.txt"
+#find . -name "*\.properties" -type f > "${PROJECT_TMP_DIR}/properties_files.txt"
 
 echo "Removes all the comments from non English files"
-sed -i '/en.properties/d' "${PROJECT_TMP_DIR}/properties_files.txt"
-cat "${PROJECT_TMP_DIR}/properties_files.txt"| while read -r FILEPATH
+sed -i '/en.properties/d' "${FULL_LIST_PATH}"
+echo "Remove comments from the following list"
+cat "${FULL_LIST_PATH}"
+cat "${FULL_LIST_PATH}"| while read -r FILEPATH
 do
  sed -i '/^#/d' $FILEPATH
 done
