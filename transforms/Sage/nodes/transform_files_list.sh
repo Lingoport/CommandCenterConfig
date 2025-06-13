7a594ff7329553e6b5af3ab778a9d054b53cede4
Olivier Libouban <olibouban@lingoport.com>
2025-06-13 13:19:45 -0600

    transforming the file name and the comments

--- transforms/Sage/nodes/transform_files_list.sh
+++ transforms/Sage/nodes/transform_files_list.sh
@@ -1,7 +1,30 @@
 #!/bin/bash
-# Nothing to do on the way back. 
-# The translated files are already in the right location
 #
-echo "${0}: No-Op"
-echo  " ------------------------------------------------"
+
+# Check if there is a file parameter
+#
+if [ -z "$1" ]
+  then
+    echo "Error: Missing the argument like /<path>/pseudo_files.txt"
+    exit 1
+fi
+
+if [ -f "$1" ]; then
+    echo " File to rewrite: $1"
+else
+    echo " $1 not found"
+    exit 1
+fi
+
+
 echo " "
+echo " --------------------------------------------"
+echo " Files Modified:  $1"
+echo "   >>  tran_filename.json -> filename.json"
+sed -i 's/tran_//' "$1"
+
+echo " "
+ls -l "$1"
+cat "$1"
+echo " --------------------------------------------"
+
--- transforms/Sage/nodes/transform_to_repo.sh
+++ transforms/Sage/nodes/transform_to_repo.sh
@@ -1,8 +1,22 @@
 #!/bin/bash
-#   No-Op
+# transforms tran_<file>.json into <file>.json
 #
-
-echo "${0}: No-Op"
 echo  " ------------------------------------------------"
-echo " "
+echo  " Current pwd:"
+pwd
+
+echo "Files to transform: "
+cat "${FULL_LIST_PATH}"
+
+cat "${FULL_LIST_PATH}" | while read -r FILEPATH
+do
+  FILENAME=`basename $FILEPATH`
+  DIRNAME=`dirname $FILEPATH`
+  TARGET_NAME="${FILENAME#tran_}"
+  TARGET_PATH="${DIRNAME}/${TARGET_NAME}"
+  echo "   Transform [$FILENAME] -> [$TARGET_NAME]"
+
+  cp "$FILENAME" "$TARGET_NAME"
+  sed -i "s/\"_tran-//" "$TARGET_NAME"
+done
 
