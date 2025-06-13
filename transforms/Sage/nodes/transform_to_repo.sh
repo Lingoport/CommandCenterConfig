b6e722d4b906c15bf75f3ae5e2acd61cfc311cef
Olivier Libouban <olibouban@lingoport.com>
2025-06-13 13:49:09 -0600

    different prefix, only tran_

--- transforms/Sage/nodes/transform_to_repo.sh
+++ transforms/Sage/nodes/transform_to_repo.sh
@@ -14,9 +14,9 @@ do
   DIRNAME=`dirname $FILEPATH`
   TARGET_NAME="${FILENAME#tran_}"
   TARGET_PATH="${DIRNAME}/${TARGET_NAME}"
-  echo "   Transform [$FILENAME] -> [$TARGET_NAME]"
+  echo "   Transform [$FILEPATH] -> [$TARGET_PATH]"
 
-  cp "$FILENAME" "$TARGET_NAME"
-  sed -i "s/\"_tran-//" "$TARGET_NAME"
+  cp "$FILEPATH" "$TARGET_PATH"
+  sed -i "s/\"tran_//" "$TARGET_PATH"
 done
 
