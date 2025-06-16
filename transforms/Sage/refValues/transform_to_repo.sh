a5ee6acedec6ad3e20b65626ff66d8cc3e36506a
Olivier Libouban <olibouban@lingoport.com>
2025-06-16 13:03:16 -0600

    transform file list and to repo: remove the _ to _title and _description

--- transforms/Sage/refValues/transform_to_repo.sh
+++ transforms/Sage/refValues/transform_to_repo.sh
@@ -1,8 +1,31 @@
 #!/bin/bash
-#   No-Op
+# transforms tran_<file>.json into <file>.json
 #
-
-echo "${0}: No-Op"
 echo  " ------------------------------------------------"
-echo " "
+echo  " Current pwd:"
+pwd
+
+echo "Files to transform: "
+cat "${FULL_LIST_PATH}"
+
+cat "${FULL_LIST_PATH}" | while read -r FILEPATH
+do
+  FILENAME=`basename $FILEPATH`
+  DIRNAME=`dirname $FILEPATH`
+  TARGET_NAME="${FILENAME#tran_}"
+  TARGET_PATH="${DIRNAME}/${TARGET_NAME}"
+  echo "   Transform [$FILEPATH] -> [$TARGET_PATH]"
+
+  # get the target file ready
+  cp "$FILEPATH" "$TARGET_PATH"
+
+  # change the "_title" keys into "title" 
+  #
+  sed -i "s/\"_title\"/\"title\"/" "$TARGET_PATH"
+
+  # change the "_description" keys into "description" 
+  #
+  sed -i "s/\"_description\"/\"description\"/" "$TARGET_PATH"
+
+done
 
