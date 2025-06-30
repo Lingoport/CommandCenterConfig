80fcf594894993df73cb8d6f5b7f4c3286fea088
Olivier Libouban <olibouban@lingoport.com>
2025-06-30 12:49:21 -0600

    Copy the file, not the file/*

--- transforms/to_lproj/transform_to_repo.sh
+++ transforms/to_lproj/transform_to_repo.sh
@@ -5,7 +5,7 @@
 echo  " ------------------------------------------------"
 echo  " Current pwd:"
 pwd
-echo  "  Copy the files from <locale>.lproj/* to <locale>/"
+echo  "  Copy the files from <locale>.lproj/translation.strings <locale>/translation.strings"
 
 echo "Files to transform (copy): "
 cat "${FULL_LIST_PATH}"
@@ -16,9 +16,9 @@ cat "${FULL_LIST_PATH}" | while read -r FILEPATH
 do
 
   TARGET_PATH="${FILEPATH//$LPROJ/}"
-  echo " copy files in [${FILEPATH}] to [$TARGET_PATH]"
+  echo " Copy [${FILEPATH}] to [$TARGET_PATH]"
 
-  cp "${FILEPATH}"/* "${TARGET_PATH}"
+  cp "${FILEPATH}" "${TARGET_PATH}"
 
 done
 
