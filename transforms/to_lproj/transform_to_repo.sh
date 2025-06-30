65209c0e4339600738f8ea2bd26f841b2ef244f6
Olivier Libouban <olibouban@lingoport.com>
2025-06-30 12:56:23 -0600

    Silent mkdir

--- transforms/to_lproj/transform_to_repo.sh
+++ transforms/to_lproj/transform_to_repo.sh
@@ -16,6 +16,8 @@ cat "${FULL_LIST_PATH}" | while read -r FILEPATH
 do
 
   TARGET_PATH="${FILEPATH//$LPROJ/}"
+  TARGET_DIR=$(dirname $TARGET_PATH)
+  mkdir -p "${TARGET_PATH}"
   echo " Copy [${FILEPATH}] to [$TARGET_PATH]"
 
   cp "${FILEPATH}" "${TARGET_PATH}"
